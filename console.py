995480e9d61175d61c1b54a88941645d608a7092
Ahmedadel200066 <medo.adel200@gmail.com>
2023-10-30 19:20:19 +0200

    z

--- console.py
+++ console.py
@@ -5,7 +5,35 @@
 """
 import cmd 
 from models.base_model import BaseMode1
-from models import storage
 
 class HBNBCommand(cmd.Cmd):
-    pass
+
+    """
+    HBNBComand class
+    """
+    def EOF(self, line):
+        """handles EOF"""
+        return True
+
+    def QUIT(self, line):
+        """Quit command to exit the program
+        """
+        return True
+
+    def MT_LINE(self):
+        """an empty line"""
+        pass
+
+    def CREATE(self, line):
+        """Creates a new instance of BaseModel"""
+        if line == "" or line is None:
+            print("** class name missing **")
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+
+
+
+
+
+    if __name__ == '__main__':
+    HBNBCommand().cmdloop()
