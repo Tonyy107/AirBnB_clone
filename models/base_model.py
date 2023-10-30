b288a7114d56d20f634a3956e260bf2deda052e1
Tonyy107 <antonmagdy53@gmail.com>
2023-10-30 19:04:12 +0200

    lol

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,16 @@ from datetime import datetime
 
 
 class BaseModel:
+    """BaseModel class"""
     id = str(uuid.uuid4())
     created_at = datetime.now()
     updated_at = created_at
+
+    def __str__(self):
+        """str """
+        return "[{}] ({}) {}".format(__name__, id, __dict__)
+
+    def save(self):
+        """save method"""
+        self.updated_at = datetime.now()
+        
