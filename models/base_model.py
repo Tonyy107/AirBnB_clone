5fba38fe410416b52a4b58cb73f57354f2d6a3e7
Tonyy107 <antonmagdy53@gmail.com>
2024-08-03 14:17:22 +0300

    Refactor `to_dict` method in `base_model.py` to return a dictionary representation of the object with formatted date strings for `created_at` and `updated_at` properties.

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 from datetime import datetime
 
+
 def save(self):
+    """
+    Updates the `updated_at` attribute with the current datetime.
+    """
     self.updated_at = datetime.now()
 
+
 def to_dict(self):
+    """
+    Converts the object to a dictionary representation.
+
+    Returns:
+        dict: A dictionary representation of the object.
+    """
     self.updated_at = self.updated_at.strftime("%d/%m/%Y %H:%M:%S")
     self.created_at = self.created_at.strftime("%d/%m/%Y %H:%M:%S")
     return self.__class__.__dict__
